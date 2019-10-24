a301708f8fdf7dfb6f67ef82c61cab448ccf85d9
LC-daoxin <457121827@qq.com>
2019-10-24 13:03:50 +0800

    Header

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import App from './App'
 import router from './router'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <div class="header-left">1</div>
+    <div class="header-input">2</div>
+    <div class="header-right">3</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+  .header{
+    display: flex;
+    height: .86rem;
+    line-height: .86rem;
+    background: #00bcd4;
+    color: #fff;
+    .header-left{
+      width: .64rem;
+      float: left;
+    }
+    .header-input{
+      flex: 1;
+      margin-top: .12rem;
+      height: .64rem;
+      line-height: .64rem;
+      background: #fff;
+      border-radius: .1rem;
+      color: #ccc;
+    }
+    .header-right{
+      width: 1.24rem;
+      float: right;
+      text-align: center;
+    }
+  }
+</style>
