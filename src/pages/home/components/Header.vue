36e7aa30bc6a8bbb3f2cf2b2bff08e5ee9fd7884
LC-daoxin <457121827@qq.com>
2019-10-24 14:09:52 +0800

    import scss

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-left">1</div>
-    <div class="header-input">2</div>
-    <div class="header-right">3</div>
+    <div class="header-left"><div class="iconfont back-icon">&#xe624;</div></div>
+    <div class="header-input"><span class="iconfont search-icon">&#xe62f;</span>输入城市/景点/游玩主题</div>
+    <div class="header-right">城市<span class="iconfont city-icon">&#xe64a;</span></div>
   </div>
 </template>
 
@@ -13,29 +13,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '../../../assets/styles/variables.scss';
   .header{
     display: flex;
     height: .86rem;
     line-height: .86rem;
-    background: #00bcd4;
+    background: $bgColor;
     color: #fff;
     .header-left{
       width: .64rem;
       float: left;
+      .back-icon{
+        text-align: center;
+        font-size: .4rem;
+      }
     }
     .header-input{
       flex: 1;
       margin-top: .12rem;
       height: .64rem;
+      padding-left: .3rem;
       line-height: .64rem;
       background: #fff;
       border-radius: .1rem;
       color: #ccc;
+      .search-icon{
+        margin-right: .1rem;
+      }
     }
     .header-right{
       width: 1.24rem;
       float: right;
       text-align: center;
+      .city-icon{
+        font-size: .22rem;
+        margin-left: .04rem;
+      }
     }
   }
 </style>
