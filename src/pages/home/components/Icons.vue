b9dfaab5af7ee4e324010d18930d91580aa3ee94
LC-daoxin <457121827@qq.com>
2019-10-26 21:29:23 +0800

    add Recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -85,6 +85,7 @@ export default {
   @import '~@styles/variables.scss';
   @import '~@styles/mixins.scss';
   .icons{
+    margin-top: .1rem;
     >>> .swiper-container{
       height: 0;
       padding-bottom: 50%;
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="">
+    <div class="recommend-title">热销推荐</div>
+    <ul class="recommend-ul">
+      <li class="item" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl"/>
+        <div class="item-ifo">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <div class="item-bottom">
+            <button class="item-button">查看详情</button>
+            <p class="item-addr">{{item.address}}</p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: require('@img/recommend1.jpg'),
+        title: '子胥湖·欢乐嘉年华',
+        desc: '浪漫的主题游乐园',
+        address: '郧阳区'
+      }, {
+        id: '0002',
+        imgUrl: require('@img/recommend2.jpg'),
+        title: '野人谷',
+        desc: '浪漫的主题游乐园',
+        address: '房山'
+      }, {
+        id: '0003',
+        imgUrl: require('@img/recommend3.jpg'),
+        title: '太极峡风景区',
+        desc: '浪漫的主题游乐园',
+        address: '丹江口市'
+      }, {
+        id: '0004',
+        imgUrl: require('@img/recommend4.jpg'),
+        title: '武当太极湖',
+        desc: '浪漫的主题游乐园',
+        address: '丹江口市'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "~@styles/mixins.scss";
+  .recommend-title{
+    margin-top: .2rem;
+    line-height: .8rem;
+    background: #eee;
+    text-indent: .2rem;
+  }
+  .recommend-ul{
+    margin-left: .2rem;
+    .item{
+      padding: .2rem 0;
+      overflow: hidden;
+      display: flex;
+      height: 2rem;
+      border-bottom: 1px solid #eee;
+      .item-img{
+        width: 2rem;
+        height: 2rem;
+      }
+      .item-ifo{
+        flex: 1;
+        padding-left: .2rem;
+        min-width: 0;
+        .item-title{
+          line-height: .54rem;
+          margin-top: .24rem;
+          font-size: .36rem;
+          @include ellipsis;
+        }
+        .item-desc{
+          line-height: .4rem;
+          color: #ccc;
+          @include ellipsis;
+        }
+        .item-bottom{
+          position: relative;
+          height: .82rem;
+          line-height: .82rem;
+          .item-button{
+            position: absolute;
+            left: 0;
+            line-height: .44rem;
+            margin-top: .2rem;
+            background:  #ff9300;
+            padding:  0 .2rem;
+            border-radius: .1rem;
+            color: #fff;
+          }
+          .item-addr{
+            position: absolute;
+            right: .24rem;
+            font-size: .2rem;
+            color: #ccc;
+          }
+        }
+      }
+    }
+  }
+</style>
