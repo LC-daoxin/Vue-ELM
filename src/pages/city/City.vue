e148d0cdde991fc13e82ff56995a21a805abe54d
LC-daoxin <457121827@qq.com>
2019-10-28 13:58:46 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitysSearch'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "~@styles/variables.scss";
+  .search{
+    height: .72rem;
+    padding: 0 .1rem;
+    background: $bgColor;
+    .search-input{
+      box-sizing: border-box;
+      width: 100%;
+      height: .62rem;
+      line-height: .62rem;
+      padding: 0 .1rem;
+      text-align: center;
+      border-radius: .06rem;
+      color: #666;
+    }
+  }
+</style>
