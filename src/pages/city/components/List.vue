0e94930a6aa435b682ff178151869a532a63f4f0
LC-daoxin <457121827@qq.com>
2019-10-28 18:19:42 +0800

    add touchmove

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,12 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+      >
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
@@ -33,10 +38,19 @@ export default {
   name: 'CitysList',
   props: {
     cities: Object,
-    hot: Array
+    hot: Array,
+    letter: String
   },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element, 300)
+      }
+    }
   }
 }
 </script>
