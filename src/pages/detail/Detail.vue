ed8ecf1dcd3cf125af77cde825ba0da77e51711b
LC-daoxin <457121827@qq.com>
2019-10-30 15:04:43 +0800

    recursion

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,55 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票'
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票',
+        children: [{
+          title: '学生三馆联票'
+        }, {
+          title: '学生五馆联票'
+        }]
+      }, {
+        title: '儿童票',
+        children: [{
+          title: '儿童三馆联票'
+        }, {
+          title: '儿童五馆联票'
+        }]
+      }, {
+        title: '家庭票',
+        children: [{
+          title: '家庭三馆联票'
+        }, {
+          title: '家庭五馆联票'
+        }]
+      }]
+    }
   }
 }
 </script>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -72,6 +72,7 @@ export default {
     }
   }
   .header-fixed{
+    z-index: 2;
     position: fixed;
     top: 0;
     left: 0;
